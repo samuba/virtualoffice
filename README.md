58fd2178a212983e280343d41033d6372f2b6584
Bach <bachsam@emea.corpdir.net>
2020-03-19 14:59:03 +0100

    first

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ Virtual-Office
 =========================
 
 Get a feeling of presence for your remote working colleagues as if they were in the same room as you.
-wanna know who is afk or eating right now so you can join? Just have a look in the Virtual-Office.
+Wanna know who is afk or eating right now so you can join? Just have a look in the Virtual-Office!
 
-What id does:
+What it does:
 Update every 5 seconds the webcam pictures of users in the room. 
 
 Use it: [virtual-office.azurewebsites.net](https://virtual-office.azurewebsites.net)
--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const compression = require('compression');
 
 app.use(compression())
 app.use(bodyParser.json())
+app.use(bodyParser.raw())
+app.use(bodyParser.text())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static("public"));
 
 const rooms = {
   testRoom: {
     users: {
-     user1: { imageData: "", updatedAt: new Date() }
-    }  
+     user1: { imageData: "", updatedAt: new Date(), name:"tester" }
+    },
+    chat: getArrayWithLimitedLength(3)
   },
 };
 
@@ -39,6 +42,23 @@ app.put("/rooms/:roomId/:userId", (req, res) => {
   res.sendStatus(200);
 });
 
+app.put("/rooms/:roomId/:userId/chat", (req, res) => {
+  const roomId = req.params.roomId
+  const userId = req.params.userId
+  const room = rooms[roomId] || createRoom(roomId)
+  postChatMessage(room, room.users[userId], req.body); 
+  res.json(room.chat);
+});
+
+function postChatMessage(room, user, message) {
+  console.log("new message", user)
+  room.chat.push({
+    authorName: user.name,
+    message: message,
+    createdAt: new Date(),
+  })
+}
+
 function purgeOldUsers(room) {
   for(let user in room.users) {
     if ((new Date() - room.users[user].updatedAt) > (1000 * 20)) {
@@ -51,16 +71,29 @@ function purgeOldUsers(room) {
 function setUser(room, userId, imageData) {
   room.users[userId] = {
     imageData: imageData,
-    updatedAt: new Date()
+    updatedAt: new Date(),
+    name: userId
   }
   return room.users[userId]
 }
 
 function createRoom(roomId) {
   rooms[roomId] = {
-    users: {}
+    users: {},
+    chat: getArrayWithLimitedLength(500)
   };
   return rooms[roomId];
+}
+
+function getArrayWithLimitedLength(length) {
+    var array = new Array();
+    array.push = function () {
+        if (this.length >= length) {
+            this.shift();
+        }
+        return Array.prototype.push.apply(this,arguments);
+    }
+    return array;
 }
 
 // listen for requests :)
